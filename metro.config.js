1ad8c1ae28ecf398e9ba67f35555169b6410dca1
anhvu5 <anh.vu5@mservice.com.vn>
2022-07-07 13:39:07 +0700

    push code

--- metro.config.js
+++ metro.config.js
@@ -15,3 +15,14 @@ module.exports = {
     }),
   },
 };
+
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+// exclusionList is a function that takes an array of regexes and combines
+// them with the default exclusions to return a single regex.
+
+module.exports = {
+  resolver: {
+    blacklistRE: exclusionList([/amplify\/.*/])
+  }
+};
