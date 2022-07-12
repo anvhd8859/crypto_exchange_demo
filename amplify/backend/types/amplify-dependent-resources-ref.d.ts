d00047e320ec5a9f6dd61285ea755f0a678ccfb4
anhvu5 <anh.vu5@mservice.com.vn>
2022-07-12 23:40:06 +0700

    push code

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,31 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "mcryptov137e05cd3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "GoogleWebClient": "string"
+        }
+    },
+    "function": {
+        "mcryptov137e05cd3PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "mcryptov1": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      type
+      email
+      name
+      image
+      netWorth
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      type
+      email
+      name
+      image
+      netWorth
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      type
+      email
+      name
+      image
+      netWorth
+      createdAt
+      updatedAt
+    }
+  }
+`;
